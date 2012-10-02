82576852d17de5e73e059b13bb827d413f8748a3
Tom Arild Pedersen <tom@gmail.com>
2012-10-02 15:17:41 +0200

    test2

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,7 +13,7 @@ namespace Test
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            Application.EnableVisualStyles();  
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
